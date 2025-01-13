61dd676fd9b776cd364b947c6b78f67fb618b813
LinAung-Thet <laungthet@gmail.com>
2025-01-13 21:33:09 +1300

    Added the solution
    Added EF, Identity Framework and gRPC, and implemented the authentication over gRPC message commands.

---
+++ gRPC_Test/Grpc_Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using GrpcShared;
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Alice" });
+
+Console.WriteLine($"Greeting: {reply.Message}");

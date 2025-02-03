d86c15a355f577bd2153e0055b99f373fc3fa629
LinAung-Thet <laungthet@gmail.com>
2025-02-03 22:01:06 +1300

    Application Layer - Added integration events for basket creation

---
+++ DomainDrivenDesign/BasketContext.Application/Events/Integration/BasketCreatedIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using BasketContext.Domain.Baskets;
+using Common.Core.Events;
+using Common.Domain;
+
+namespace BasketContext.Application.Events.Integration
+{
+    public sealed class BasketCreatedIntegrationEvent : IntegrationEvent
+    {
+        public BasketCreatedIntegrationEvent(Id<Basket> basketId, Guid customerId)
+            : base(basketId)
+        {
+            CustomerId = customerId;
+        }
+        public BasketCreatedIntegrationEvent() { }
+
+        public Guid CustomerId { get; set; }
+    }
+}
--- DomainDrivenDesign/BasketContext.Application/obj/Debug/net9.0/BasketContext.Application.AssemblyInfo.cs
+++ DomainDrivenDesign/BasketContext.Application/obj/Debug/net9.0/BasketContext.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasketContext.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b256a6f2fe3f18d57e4a15fb55087cc2f1d156e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd7e6e914a101dbecd168bb029c6029bf077ae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasketContext.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasketContext.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

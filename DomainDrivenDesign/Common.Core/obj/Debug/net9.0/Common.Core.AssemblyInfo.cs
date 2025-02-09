f02d2efbfeffefdc1a192c0107fcc69ced02e82b
LinAung-Thet <laungthet@gmail.com>
2025-02-09 21:27:57 +1300

    Infrastructure Layer
    - Added a start-up project for the infrastructure layer

--- DomainDrivenDesign/Common.Core/obj/Debug/net9.0/Common.Core.AssemblyInfo.cs
+++ DomainDrivenDesign/Common.Core/obj/Debug/net9.0/Common.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c38192b04d14a4bda6e4e2b9e838b6e45441b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0424b384891d008592c48ceda4eb4322eb9732")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

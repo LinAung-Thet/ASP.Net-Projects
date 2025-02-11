555336cc28dc350b86fa8b0f68fc297a4e0d283d
LinAung-Thet <laungthet@gmail.com>
2025-02-11 21:26:36 +1300

    Infrastructure Layer
    - Added basket, basket item, coupon, customer, and seller entities.

---
+++ DomainDrivenDesign/BasketContext.Infrastructure/Persistence/Entities/CustomerEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace BasketContext.Infrastructure.Entities
+{
+    public class CustomerEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsEliteMember { get; set; }
+        public ICollection<BasketEntity> Baskets { get; set; }
+    }
+}
